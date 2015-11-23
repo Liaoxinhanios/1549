9c0b2b0d8846c695b1b86aab4d078d8f9b72a243
qianfeng <qianfeng@123-5.local>
2015-11-23 17:13:27 +0800

    v3

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)HelloWorld {
+    
+    NSLog(@"hello world");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
